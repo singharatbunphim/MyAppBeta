f32e1dc3af276d6642d5942c0aaf39b74501bcc9
singharatbunphim <singharatbunphim@gmail.com>
2022-07-20 22:37:32 +0700

    add Navbar scroll down event

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,14 +1,41 @@
-import styles from '../styles/navbar.module.scss';
+import styles from '../styles/navbar.module.scss'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React , { useState , useEffect } from 'react'
 
 const Navbar = () => {
+
+    const [isScrollDown , setScrollDown ] = useState(false);
+
+    const HandleScroll = () => {
+        const ofset = window.scrollY;
+
+
+        if( ofset > 200 ) {
+            setScrollDown(true)
+        }else {
+            setScrollDown(false)
+        }
+
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => HandleScroll() );
+
+    }, [isScrollDown])
+    
+
+    const router = useRouter();
+
+
     return (
-        <nav className={styles.navbar}>
+        <nav className={`${isScrollDown ? `${styles.active} ` :  null } ${styles.navbar}`}>
             <div className={styles.logo}>MyLogo</div>
             <div className={styles.menubar}>
                 <ul>
-                    <li><Link href="/"><a>Home</a></Link></li>
-                    <li><Link href="/about"><a>About</a></Link></li>
+                    <li className={`${router.pathname == "/" ? `${styles.active}` : null }`}><Link href="/"><a>Home</a></Link></li>
+                    <li className={`${router.pathname == "/about" ? `${styles.active}` : null }`}><Link href="/about"><a>About</a></Link></li>
                     <li><Link href="/contact"><a>Contact</a></Link></li>
                     <li><Link href="/project"><a>Project</a></Link></li>
                 </ul>
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,14 @@
+import Footer from "../component/Footer";
+import styles from '../styles/main.module.scss'
+import Navbar from "../component/Navbar";
+
 const about = () => {
     return (
         <div>
+            <Navbar />
             <h1>Hello About</h1>
-            <span>Cillum voluptate nostrud ullamco deserunt eu. Eiusmod consequat exercitation ad labore eu laboris nisi voluptate nulla enim duis. Do sunt deserunt veniam magna reprehenderit reprehenderit aute labore enim et culpa veniam. Culpa aliqua deserunt exercitation ea nisi commodo ipsum reprehenderit nulla ipsum laboris occaecat aliqua ad. Nulla sit deserunt velit do officia nisi velit. Aute consequat aliquip et commodo ex non ea quis exercitation ipsum irure. Officia velit dolor elit magna qui ut irure exercitation in.</span>
+            <section className={styles.section}>Good Homework</section>
+            <Footer />
         </div>
     )
 }
